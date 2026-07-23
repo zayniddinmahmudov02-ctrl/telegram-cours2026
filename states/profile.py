9eaa417f1e8fd27dfdfb03f297bbc422e279acd1
zayniddinmahmudov02-ctrl <zayniddin.mahmudov.02@gmail.com>
2026-07-24 02:11:36 +0500

    add

--- states/profile.py
+++ states/profile.py
@@ -1,4 +1,5 @@
 from aiogram.fsm.state import State, StatesGroup
 
+
 class ProfileState(StatesGroup):
-    waiting_for_photo = State()
+    change_name = State()
--- states/register.py
+++ states/register.py
@@ -1,6 +1,5 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class RegisterState(StatesGroup):
-    waiting_for_name = State()
-    waiting_for_phone = State()
+class RegisterStates(StatesGroup):
+    waiting_full_name = State()
